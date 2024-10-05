91af020f8a5f319757eaf192092018850609cc83
Duy Thanh <tttranduy999@gmail.com>
2024-10-05 14:59:34 +0700

    update code to deploy server

--- src/configs/storage.ts
+++ src/configs/storage.ts
@@ -5,7 +5,28 @@ import env from "dotenv";
 env.config();
 
 const CONFIG: any = process.env.JSONCONFIG_STORAGE;
-const serviceAccount = require(`../../${CONFIG}`);
+const CONFIGDATA: any = process.env.SDKCONFIG;
+const TESTURRI: any = process.env.URI;
+// const serviceAccount = require(`../../${CONFIG}`);
+
+const { private_key } = JSON.parse(process.env.PRIVATEKEY || "");
+
+console.log("DEBUG private_key: ", private_key);
+
+const serviceAccount = {
+  type: process.env.TYPE || "",
+  project_id: process.env.PROJECTID || "",
+  private_key_id: process.env.PRIVATEKEYID || "",
+  private_key: private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENTEMAIL || "",
+  client_id: process.env.CLIENTID || "",
+  auth_uri: process.env.AUTHURI || "",
+  token_uri: process.env.TOKENURI || "",
+  auth_provider_x509_cert_url: process.env.AUTHPROVIDERX509CERTURL || "",
+  client_x509_cert_url: process.env.CLIENTX509CERTURL || "",
+  universe_domain: process.env.UNIVERSEDOMAIN || "",
+};
+
 const BUCKET: any = process.env.BUCKET;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
