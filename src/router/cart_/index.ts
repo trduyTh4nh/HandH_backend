3a9d09c40df06da3d1d4c474d148f6c35da6a0ed
Duy Thanh <tttranduy999@gmail.com>
2024-11-10 12:08:03 +0700

    update api remove product in cart

--- src/router/cart_/index.ts
+++ src/router/cart_/index.ts
@@ -2,11 +2,21 @@ import { Router } from "express";
 import { authentication } from "../../auth/auth";
 import { asyncHandler } from "../../helper/asyncHandler";
 import cartController from "../../controller/cart.controller";
-const router = Router()
+const router = Router();
 /// nhớ thêm api lấy toàn bộ sản phẩm
-router.post('/createCart', asyncHandler(cartController.createCart))
-router.patch('/addProductToCart', asyncHandler(cartController.addProductToCart))
-router.delete('/removeProductInCart', asyncHandler(cartController.removeProductInCart))
-router.put('/decreaseQuantityProductIncart', asyncHandler(cartController.decreaseQuantity))
-router.get('/getCartUser/:id', asyncHandler(cartController.getCartOfUser))
-export default router
+router.use(authentication);
+router.post("/createCart", asyncHandler(cartController.createCart));
+router.patch(
+  "/addProductToCart",
+  asyncHandler(cartController.addProductToCart)
+);
+router.post(
+  "/removeProductInCart",
+  asyncHandler(cartController.removeProductInCart)
+);
+router.put(
+  "/decreaseQuantityProductIncart",
+  asyncHandler(cartController.decreaseQuantity)
+);
+router.get("/getCartUser/:id", asyncHandler(cartController.getCartOfUser));
+export default router;
